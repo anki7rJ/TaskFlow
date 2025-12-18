37ec15d25b4a0468f305ef0415053c3f1e3d7fe3
Ankit Raj <167684506+ctrlCraft01@users.noreply.github.com>
2025-12-19 00:10:59 +0530

    ui: close create task modal with escape key

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -1,13 +1,32 @@
+import { useEffect } from "react";
+
 export default function Modal({ children, onClose }) {
+
+    useEffect(()=>{
+      function handleKeyDown(e){
+        if(e.key==='Escape'){
+          onClose()
+        }
+      }
+
+      window.addEventListener("keydown",handleKeyDown)
+
+      return ()=>{
+        window.removeEventListener("keydown",handleKeyDown)
+      }
+    },[onClose])
+
+    
+
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center">
-      {/* Backdrop */}
+      
       <div
         className="absolute inset-0 bg-black/40 backdrop-blur-sm"
         onClick={onClose}
       />
 
-      {/* Modal content */}
+      
       <div className="relative z-10 w-full max-w-lg rounded-lg bg-white p-6 shadow-lg">
         {children}
       </div>
